c1eaf617a384e5466b9770b02ab6918e6fd4fa73
david <el16dmcr@leeds.ac.uk>
2022-08-22 09:56:41 +0100

    created dynamic linear interpolation code

--- Reaching/dynamicLinearInterpolation.py
+++ Reaching/dynamicLinearInterpolation.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 from platform import python_version
 import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
-from sklearn.model_selection import train_test_split
 import time
 
 dof = 7
@@ -14,17 +12,38 @@ numTrajectoriesTest = 10
 sizeofMatrix = dof * 2 * dof 
 inputSize = (2 * sizeofMatrix) + 1 + (2 * dof)
 
-def createListReupdatePoints(trajectoryStates, trajectoryControls, minN, maxN):
+def createListReupdatePoints(trajectoryStates, trajectoryControls, minN, maxN, velGradSensitivty):
     reEvaluatePoints = []
     counterSinceLastEval = 0
     reEvaluatePoints.append(0)
 
+    currentGrads = np.zeros(2*dof)
+    lastGrads = np.zeros(2*dof)
+    first = True
+
     for i in range(trajecLength):
+        
+        # if(i == 0):
+        #     lastGrads = currentGrads.copy()
 
         if(counterSinceLastEval >= minN):
-            if(newEvaluationNeeded(trajectoryStates[i], trajectoryStates[i-1], 0.4)):
+
+            currState = trajectoryStates[i,:].copy()
+            lastState = trajectoryStates[i-minN,:].copy()
+
+            currentGrads = currState - lastState
+
+            if(first):
+                first = False
                 reEvaluatePoints.append(i)
                 counterSinceLastEval = 0
+            else:
+                if(newEvaluationNeeded(currentGrads, lastGrads, velGradSensitivty)):
+                    reEvaluatePoints.append(i)
+                    counterSinceLastEval = 0
+
+            lastGrads = currentGrads.copy()
+            
 
         if(counterSinceLastEval >= maxN):
             reEvaluatePoints.append(i)
@@ -36,24 +55,29 @@ def createListReupdatePoints(trajectoryStates, trajectoryControls, minN, maxN):
 
     return reEvaluatePoints
 
-def newEvaluationNeeded(currentState, lastState, sensitivity):
+def newEvaluationNeeded(currentGrads, lastGrads, sensitivity):
     newEvalNeeded = False
 
     for i in range(dof):
-        velDiff = currentState[dof + i] = lastState[dof + i]
+        # print("current grad: " + str(currentGrads[dof + i]))
+        # print("last grad: " + str(lastGrads[dof + i]))
+        velGradDiff = currentGrads[dof + i] - lastGrads[dof + i]
+        # print(velGradDiff)
 
-        if(velDiff > sensitivity):
+        if(velGradDiff > sensitivity):
             newEvalNeeded = True
+            #print("new eval needed, diff: " + str(velGradDiff))
 
-        if(velDiff < -sensitivity):
+        if(velGradDiff < -sensitivity):
             newEvalNeeded = True
+            #print("new eval needed, diff: " + str(velGradDiff))
 
 
     return newEvalNeeded
 
 def generateLinearInterpolationData(A_matrices, reEvaluationIndicies):
     sizeofAMatrix = len(A_matrices[0])
-    linInterpolationData = np.zeros((trajecLength - 1, sizeOfAMatrix))
+    linInterpolationData = np.zeros((trajecLength - 1, sizeofAMatrix))
 
     numBins = len(reEvaluationIndicies) - 1
     print("num bins: " + str(numBins))
@@ -61,7 +85,7 @@ def generateLinearInterpolationData(A_matrices, reEvaluationIndicies):
 
     for i in range(numBins):
 
-        for j in range(sizeOfAMatrix):
+        for j in range(sizeofAMatrix):
 
             startIndex = reEvaluationIndicies[i]
             startVals = A_matrices[startIndex,:]
@@ -83,36 +107,130 @@ def generateLinearInterpolationData(A_matrices, reEvaluationIndicies):
 
     return linInterpolationData
 
+def evaluateInterpolation(testTrajectories, states, controls):
+    meanSSE_lin = 0 
 
+    SSD_lin = np.zeros((len(testTrajectories)))
+    numBins = np.zeros((len(testTrajectories)))
+    avgBins = 0
+
+    linInterpolationData = []
+
+    # loop over the test trajectories
+    for j in range(len(testTrajectories)):
+
+        reEvaluationIndicies = createListReupdatePoints(states[j], controls[j], 5, 200, 0.01)
+        numBins[j] = len(reEvaluationIndicies)
+        print(reEvaluationIndicies)
+        linInterpolationData = generateLinearInterpolationData(testTrajectories[j], reEvaluationIndicies)
+
+        SSD_lin[j] = calcMeanSumSquaredDiffForTrajec(linInterpolationData, testTrajectories[j])
+        print("SSD_lin was: " + str(SSD_lin[j]))
+
+        plt.plot(testTrajectories[j][:, 5])
+        plt.plot(linInterpolationData[:, 5])
+        plt.show()
+
+    return SSD_lin, numBins
+
+def calcMeanSumSquaredDiffForTrajec(groundTruth, prediction):
+    size = len(groundTruth[0])
+
+    array1Size = len(groundTruth)
+    array2Size = len(prediction)
+    lenofTrajec = array2Size
+    
+    if(array1Size < array2Size):
+        lenofTrajec = array1Size
+    
+    sumSqDiff = np.zeros((lenofTrajec))
+
+    for i in range(lenofTrajec):
+        diffVals = np.zeros((size))
+        
+        for j in range(size):
+
+            diffVals[j] = groundTruth[i, j] - prediction[i, j]
+            sumSqDiff[i] = sumSqDiff[i] + (diffVals[j] * diffVals[j])
+
+        # print(sumSqDiff[i])
+
+    #print(sumSqDiff[0:10])
+
+    meanSumSquared = np.mean(sumSqDiff)
+
+    return meanSumSquared 
+
+
+def plottingScatter():
+    pandas = pd.read_csv('error_lin.csv', header=None)
+    error_lin = pandas.to_numpy()
+
+    pandas = pd.read_csv('error_quad.csv', header=None)
+    error_quad = pandas.to_numpy()
+
+    pandas = pd.read_csv('numEvalsDynamicsLin.csv', header=None)
+    numEvalsLin = pandas.to_numpy()
+
+    pandas = pd.read_csv('sumSquaredDiffsDynamicLin.csv', header=None)
+    error_dynlin = pandas.to_numpy()
+
+    normalEvals = np.array([1500, 600, 300, 150, 60, 30, 15, 6])
+    #normalEvals = np.array([2, 5, 10, 20, 50, 100, 200, 500])
+    data_error_lin = np.zeros((len(error_lin), 2))
+    data_error_quad = np.zeros((len(error_quad), 2))
+    data_error_dynLin = np.zeros((len(error_quad), 2))
+
+    for i in range(len(error_lin)):
+        data_error_lin[i, 0] = error_lin[i]
+        data_error_lin[i, 1] = normalEvals[i]
+
+        data_error_quad[i, 0] = error_quad[i]
+        data_error_quad[i, 1] = normalEvals[i]
+
+        data_error_dynLin[i, 0] = error_dynlin[i]
+        data_error_dynLin[i, 1] = numEvalsLin[i]
 
 
 def main():
 
 
-    pandas = pd.read_csv('Data/testing_trajectories.csv', header=None)
+    pandas = pd.read_csv('testing_data/testing_trajectories.csv', header=None)
 
     testTrajectories = []
-    states = []
+    testStates = []
+    testControls = []
 
     for i in range(numTrajectoriesTest):
         testTrajectories.append([])
         tempPandas = pandas.iloc[i*3000:(i + 1)*3000]
         testTrajectories[i] = tempPandas.to_numpy()
-    
-    pandas = pd.read_csv('Data/testing_states.csv', header=None)
+
+    pandas = pd.read_csv('testing_data/testing_states.csv', header=None)
 
     for i in range(numTrajectoriesTest):
-        states.append([])
+        testStates.append([])
         tempPandas = pandas.iloc[i*3000:(i + 1)*3000]
-        states[i] = tempPandas.to_numpy()
+        testStates[i] = tempPandas.to_numpy()
+
+    pandas = pd.read_csv('testing_data/testing_controls.csv', header=None)
+
+    for i in range(numTrajectoriesTest):
+        testControls.append([])
+        tempPandas = pandas.iloc[i*3000:((i + 1)*3000)]
+        testControls[i] = tempPandas.to_numpy()
 
     print("DATA LOADED")
 
+    sumSquaredDiffs, numEvals = evaluateInterpolation(testTrajectories, testStates, testControls)
+
+    meanSSD = np.mean(sumSquaredDiffs) 
+    avgEvals = np.mean(numEvals)
+    print("mean sum sqaured diff: " + str(meanSSD))
+    print("average nuumber of evaluaions: " + str(avgEvals))
 
 
 
-
-    pass
 
 
 
